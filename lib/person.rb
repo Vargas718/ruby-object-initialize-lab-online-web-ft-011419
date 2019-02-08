42214d020460006c5e0d4fb3cfa847f373db4250
Rafael Vargas <vargas718@gmail.com>
2019-02-08 06:53:40 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,3 @@
 class Person
-
-def initilize(name)
-  @name = name
+attr_accessor :name
 end
-
-def name=(name)
-  @name = name
-end
-
-def name
-  @name
-
-end
-
-  
-end
-
-beyonce = Person.new
-beyonce.name = "Beyonce"
